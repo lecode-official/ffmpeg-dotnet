541bc3e4058a457c04740d3a2f9d4f45c4eab261
David Neumann <david.neumann@live.com>
2016-07-01 17:06:30 +0200

    Started to make it possible to open media files.

--- AVFormat.cs
+++ AVFormat.cs
@@ -20,6 +20,27 @@ namespace System.Media.FFMpeg.Interop
         [DllImport("libavformat.so")]
         public static extern void av_register_all();
 
+        /// <summary>
+        /// Open an input stream and read the header. The codecs are not opened. The stream must be closed with avformat_close_input().
+        /// </summary>
+        /// <param name="ps"> <summary>
+        /// Pointer to the user-supplied AVFormatContext (allocated by avformat_alloc_context). May be a pointer to <c>null</c>, in which case an <see cref="AVFormatContext"/> is allocated by this function and written into <see cref="ps"/> Note that
+        /// a user-supplied <see cref="AVFormatContext"/> will be freed on failure.
+        /// </summary>
+        /// <param name="url">URL of the stream to open.</param>
+        /// <param name="fmt">If non-<c>null</c>, this parameter forces a specific input format. Otherwise the format is autodetected.</param>
+        /// <param name="options">A dictionary filled with <see cref="AVFormatContext"/> and demuxer-private options. On return this parameter will be destroyed and replaced with a dictionary containing options that were not found. May be <c>null</c>.</param>
+        /// <returns>Returns 0 on success, and a negative AVERROR on failure.</returns>
+        [DllImport("libavformat.so")]
+        public static extern int avformat_open_input([Out] out IntPtr ps, [MarshalAs(UnmanagedType.LPStr)] string url, IntPtr fmt, IntPtr options);
+
+        /// <summary>
+        /// Close an opened input <see cref="AVFormatContext"/>. Free it and all its contents and set <see cref="s"/> to <c>null</c>.
+        /// </summary>
+        /// <param name="s">The <see cref="AVFormatContext"/> that is to be closed.</param>
+        [DllImport("libavformat.so")]
+        public static extern void avformat_close_input(IntPtr s);
+
         #endregion
     }
 }
---
+++ AVFormatContext.cs
@@ -0,0 +1,41 @@
+
+#region Using Directives
+
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace System.Media.FFMpeg.Interop
+{
+    /// <summary>
+    /// Represents the format I/O context.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct AVFormatContext
+    {
+        #region Public Fields
+
+        /// <summary>
+        /// Contains a class for logging and avoptions. Set by avformat_alloc_context(). Exports (de)muxer private options if they exist.
+        /// </summary>
+        public IntPtr av_class;
+
+        /// <summary>
+        /// Contains the input container format (as a pointer to an <see cref="AVInputFormat"/> struct). Demuxing only, set by avformat_open_input().
+        /// </summary>
+        public IntPtr iformat;
+
+        /// <summary>
+        /// Contains the output container format (as a pointer to an <see cref="AVOutputFormat"/> struct). Muxing only, must be set by the caller before avformat_write_header().
+        /// </summary>
+        public IntPtr oformat;
+
+        /// <summary>
+        /// Contains format private data. This is an <see cref="AVOptions"/>-enabled struct if and only if <see cref="iformat.priv_class"/>/<see cref="oformat.priv_class"/> is not <c>null</c>. When muxing it is set by avformat_write_header()
+        /// and when demuxing it is set by avformat_open_intput().
+        /// </summary>
+        public IntPtr priv_data;
+
+        #endregion
+    }
+}
