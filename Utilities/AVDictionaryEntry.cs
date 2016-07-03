dd358409f538ae0cd2bb85e0018340340e8b09dd
David Neumann <david.neumann@live.com>
2016-07-03 13:36:56 +0200

    Added the AV dictionray structure.

---
+++ Utilities/AVDictionaryEntry.cs
@@ -0,0 +1,33 @@
+
+#region Using Directives
+
+using System;
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace FFmpeg.Utilities
+{
+    /// <summary>
+    /// Represents a key-value-pair that can be stored in a <see cref="AVDictionary"/>.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct AVDictionaryEntry
+    {
+        #region Public Fields
+
+        /// <summary>
+        /// Contains the key of the key-value-pair.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string key;
+
+        /// <summary>
+        /// Contains the value of the key-value-pair.
+        /// </summary>
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string @value;
+
+        #endregion
+    }
+}
