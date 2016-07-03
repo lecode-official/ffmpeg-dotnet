a2c34176a7f36aaddb01ea064f25d12a274e88f2
David Neumann <david.neumann@live.com>
2016-07-03 17:15:17 +0200

    Now decoders can be found and codecs can be loaded.

--- Codecs/LibAVCodec.cs
+++ Codecs/LibAVCodec.cs
@@ -1,6 +1,7 @@
 
 #region Using Directives
 
+using System;
 using System.Runtime.InteropServices;
 
 #endregion
@@ -20,7 +21,40 @@ namespace FFmpeg.Codecs
         /// <returns>Returns the version of the libavcodec library.</returns>
         [DllImport(Libraries.AVCodec)]
         public static extern uint avcodec_version();
-        
+
+        /**
+        * Find a registered decoder with a matching codec ID.
+        *
+        * @param id AVCodecID of the requested decoder
+        * @return A decoder if one was found, NULL otherwise.
+        */
+
+        /// <summary>
+        /// Finds a registered decoder with a matching codec ID.
+        /// </summary>
+        /// <param name="id">The ID of the requested decoder.</param>
+        /// <returns>Returns a pointer to the decoder (of the type <see cref="AVCodec"/>) and <c>IntPtr.Zero</c> otherwise.</returns>
+        [DllImport(Libraries.AVCodec)]
+        public static extern IntPtr avcodec_find_decoder(AVCodecID id);
+
+        /// <summary>
+        /// Initialize the <see cref="AVCodecContext"/> to use the given <see cref="AVCodec"/>. Prior to using this function the context has to be allocated
+        /// with avcodec_alloc_context3(). The functions avcodec_find_decoder_by_name(), avcodec_find_encoder_by_name(), avcodec_find_decoder() and
+        /// avcodec_find_encoder() provide an easy way for retrieving a codec. Warning, this function is not thread safe. Note, Always call this function
+        /// before using decoding routines (such as avcodec_receive_frame()).
+        /// </summary>
+        /// <param name="avctx">A pointer to the <see cref="AVCodecContext"/> context to initialize.</param>
+        /// <param name="codec">
+        /// A pointer to the codec to open this context for. If a non-<c>null</c> codec has been previously passed to avcodec_alloc_context3() or for this
+        /// context, then this parameter MUST be either <c>IntPtr.Zero</c> or equal to the previously passed codec.
+        /// </param>
+        /// <param name="options">
+        /// A dictionary filled with AVCodecContext and codec-private options. On return this object will be filled with options thatwere not found.
+        /// </param>
+        /// <returns>Returns 0 when successful and a negative value on error.</returns>
+        [DllImport(Libraries.AVCodec)]
+        public static extern int avcodec_open2(IntPtr avctx, IntPtr codec, IntPtr options);
+
         #endregion 
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using FFmpeg.Formats;
 using System;
 using System.Runtime.InteropServices;
 using FFmpeg.Codecs;
+using FFmpeg.Utilities;
 
 #endregion
 
@@ -33,10 +34,8 @@ namespace FFmpeg
                 Console.WriteLine("An error occurred while opening the video.");
                 return;
             }
-
-            // Prints out a success message
             AVFormatContext formatContext = Marshal.PtrToStructure<AVFormatContext>(formatContextPointer);
-            Console.WriteLine($"Opened video: {formatContext.filename}");
+            Console.WriteLine($"Opened video file {formatContext.filename}.");
 
             // Retrieve stream information of the video
             if (LibAVFormat.avformat_find_stream_info(formatContextPointer, IntPtr.Zero) < 0)
@@ -45,14 +44,43 @@ namespace FFmpeg
                 return;
             }
 
-            // Prints out information about each frame in the video
-            Console.WriteLine($"Number streams: {formatContext.nb_streams}");
+            // Finds the first video stream in the video
+            Console.WriteLine($"Found {formatContext.nb_streams} stream(s) in the video file.");
+            int videoStreamId = -1;
             for (int i = 0; i < formatContext.nb_streams; i++)
             {
                 AVStream stream = Marshal.PtrToStructure<AVStream>(Marshal.PtrToStructure<IntPtr>(IntPtr.Add(formatContext.streams, i * IntPtr.Size)));
                 AVCodecContext codecContext = Marshal.PtrToStructure<AVCodecContext>(stream.codec);
-                Console.WriteLine(codecContext.codec_type);
+                if (codecContext.codec_type == AVMediaType.AVMEDIA_TYPE_VIDEO)
+                {
+                    videoStreamId = i;
+                    break;
+                }
             }
+            if (videoStreamId == -1)
+            {
+                Console.WriteLine("No video stream found.");
+                return;
+            }
+            AVStream videoStream = Marshal.PtrToStructure<AVStream>(Marshal.PtrToStructure<IntPtr>(IntPtr.Add(formatContext.streams, videoStreamId * IntPtr.Size)));
+            AVCodecContext videoCodecContext = Marshal.PtrToStructure<AVCodecContext>(videoStream.codec);
+
+            // Finds the decoder for the video stream
+            IntPtr codecPointer = LibAVCodec.avcodec_find_decoder(videoCodecContext.codec_id);
+            if (codecPointer == IntPtr.Zero) {
+                Console.WriteLine("The video codec is not supported.");
+                return;
+            }
+            AVCodec videoCodec = Marshal.PtrToStructure<AVCodec>(codecPointer);
+            Console.WriteLine($"Using the {videoCodec.long_name} codec to decdoe the video stream.");
+
+            // Opens the codec for the video stream
+            if (LibAVCodec.avcodec_open2(videoStream.codec, codecPointer, IntPtr.Zero) < 0)
+            {
+                Console.WriteLine("The codec {videoCodec.long_name} could not be opened.");
+                return;
+            }
+            Console.WriteLine("Successfully loaded codec.");
 
             // Closes the video again
             LibAVFormat.avformat_close_input(formatContextPointer);
