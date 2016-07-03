4b370768203366eac709e8fdd3eda7778e94fd5e
David Neumann <david.neumann@live.com>
2016-07-03 23:31:07 +0200

    Started to implement the decoding of video frames.

--- Formats/LibAVFormat.cs
+++ Formats/LibAVFormat.cs
@@ -60,6 +60,22 @@ namespace FFmpeg.Formats
         [DllImport(Libraries.AVFormat)]
         public static extern int avformat_find_stream_info(IntPtr ic, IntPtr options);
 
+        /// <summary>
+        /// Retrieves the next frame of a stream. This function returns what is stored in the file, and does not validate that what is there are valid frames for
+        /// the decoder. It will split what is stored in the file into frames and return one for each call. It will not omit invalid data between valid frames so
+        /// as to give the decoder the maximum information possible for decoding. If pkt->buf is NULL, then the packet is valid until the next av_read_frame() or
+        /// until avformat_close_input(). Otherwise the packet is valid indefinitely. In both cases the packet must be freed with av_packet_unref when it is no
+        /// longer needed. For video, the packet contains exactly one frame. For audio, it contains an integer number of frames if each frame has a known fixed
+        /// size (e.g. PCM or ADPCM data). If the audio frames have a variable size (e.g. MPEG audio), then it contains one frame. pkt->pts, pkt->dts and
+        /// pkt->duration are always set to correct values in AVStream.time_base units (and guessed if the format cannot provide them). pkt->pts can be
+        /// AV_NOPTS_VALUE if the video format has B-frames, so it is better to rely on pkt->dts if you do not decompress the payload.
+        /// </summary>
+        /// <param name="s">A pointer to the <see cref="seeAVFormatContext"/>.</param>
+        /// <param name="pkt">The packet that is benig output.</param>
+        /// <returns>Return 0 if everything went alright, a negative value other for an error, or end of file.</returns>
+        [DllImport(Libraries.AVFormat)]
+        public static extern int av_read_frame(IntPtr s, [Out] out IntPtr pkt);
+
         #endregion
     }
 }
