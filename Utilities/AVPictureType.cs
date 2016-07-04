ded99f7132ddcdd8ffd12b2d0b8d52fd7645f579
David Neumann <david.neumann@live.com>
2016-07-04 21:07:19 +0200

    Added some more fields to AVFrame.

---
+++ Utilities/AVPictureType.cs
@@ -0,0 +1,39 @@
+
+namespace FFmpeg.Utilities
+{
+    public enum AVPictureType
+    {
+        /// <summary>
+        /// The picture type is undefined.
+        /// </summary>
+        AV_PICTURE_TYPE_NONE = 0,
+        /// <summary>
+        /// The picture type is intra.
+        /// </summary>
+        AV_PICTURE_TYPE_I,
+        /// <summary>
+        /// The picture type is predicted.
+        /// </summary>
+        AV_PICTURE_TYPE_P,
+        /// <summary>
+        /// The picture type is bi-directionally predicted.
+        /// </summary>
+        AV_PICTURE_TYPE_B,
+        /// <summary>
+        /// The picture type is S(GMC)-VOP MPEG-4.
+        /// </summary>
+        AV_PICTURE_TYPE_S,
+        /// <summary>
+        /// The picture type is switching intra.
+        /// </summary>
+        AV_PICTURE_TYPE_SI,
+        /// <summary>
+        /// The picture type is switching predicted.
+        /// </summary>
+        AV_PICTURE_TYPE_SP,
+        /// <summary>
+        /// The picture type is BI.
+        /// </summary>    ///< Switching Predicted
+        AV_PICTURE_TYPE_BI
+    }
+}
