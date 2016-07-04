bc6b7c2ffe02b77dde9f3d4ba8645f8271a91e9e
David Neumann <david.neumann@live.com>
2016-07-05 00:53:58 +0200

    Finished the work on the AVFrame.

--- Utilities/AVFrame.cs
+++ Utilities/AVFrame.cs
@@ -72,6 +72,229 @@ namespace FFmpeg.Utilities
         /// </summary>
         public int key_frame;
 
+        /// <summary>
+        /// Contains the picture type of the frame.
+        /// </summary>
+        public AVPictureType pict_type;
+
+        /// <summary>
+        /// Contains the sample aspect ratio for the video frame, 0/1 if unknown/unspecified.
+        /// </summary>
+        public AVRational sample_aspect_ratio;
+
+        /// <summary>
+        /// Contains the presentation timestamp in time_base units (time when frame should be shown to user).
+        /// </summary>
+        public long pts;
+
+        /// <summary>
+        /// Contains the PTS copied from the <see cref="AVPacket"/> that was decoded to produce this frame.
+        /// </summary>
+        public long pkt_pts;
+
+        /// <summary>
+        /// Contains the DTS copied from the <see cref="AVPacket"/> that triggered returning this frame (if frame threading isn't used). This is also the
+        /// presentation time of this AVFrame calculated from only AVPacket.dts values without pts values.
+        /// </summary>
+        public long pkt_dts;
+
+        /// <summary>
+        /// Contains the picture number in bitstream order.
+        /// </summary>
+        public int coded_picture_number;
+
+        /// <summary>
+        /// Contains the picture number in display order.
+        /// </summary>
+        public int display_picture_number;
+
+        /// <summary>
+        /// Contains the quality between 1 (good) and FF_LAMBDA_MAX (bad).
+        /// </summary>
+        public int quality;
+
+        /// <summary>
+        /// Contains a pointer for some private data of the user.
+        /// </summary>
+        public IntPtr opaque;
+
+        /// <summary>
+        /// Contains an error array.
+        /// </summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public long[] error;
+
+        /// <summary>
+        /// Contains a value that, when decoding, signals how much the picture must be delayed: extra_delay = repeat_pict / (2 * fps).
+        /// </summary>
+        public int repeat_pict;
+
+        /// <summary>
+        /// Contains the content of the picture is interlaced.
+        /// </summary>
+        public int interlaced_frame;
+
+        /// <summary>
+        /// Contains if the content is interlaced, is top field displayed first.
+        /// </summary>
+        public int top_field_first;
+
+        /// <summary>
+        /// Contains a value that tells user applications that the palette has changed from previous frame.
+        /// </summary>
+        public int palette_has_changed;
+
+        /// <summary>
+        /// Contains the reordered opaque 64 bits (generally an integer or a double precision float PTS but can be anything). The user sets
+        /// AVCodecContext.reordered_opaque to represent the input at that time, the decoder reorders values as needed and sets AVFrame.reordered_opaque
+        /// to exactly one of the values provided by the user through AVCodecContext.reordered_opaque.
+        /// </summary>
+        public long reordered_opaque;
+
+        /// <summary>
+        /// Contains teh sample rate of the audio data.
+        /// </summary>
+        public int sample_rate;
+
+        /// <summary>
+        /// Contains the channel layout of the audio data.
+        /// </summary>
+        public long channel_layout;
+
+        /// <summary>
+        /// Contains the <see creaf="AVBuffer"/> references backing the data for this frame. If all elements of this array are NULL, then this frame is not
+        /// reference counted. This array must be filled contiguously -- if buf[i] is non-NULL then buf[j] must also be non-NULL for all j < i. There may be
+        /// at most one AVBuffer per data plane, so for video this array always contains all the references. For planar audio with more than
+        /// AV_NUM_DATA_POINTERS channels, there may be more buffers than can fit in this array. Then the extra AVBufferRef pointers are stored in the
+        /// extended_buf array.
+        /// </summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public IntPtr[] buf;
+
+        /// <summary>
+        /// Contains an array, that, for planar audio which requires more than AV_NUM_DATA_POINTERS AVBufferRef pointers, will hold all the references which
+        /// cannot fit into AVFrame.buf. Note, that this is different from AVFrame.extended_data, which always contains all the pointers. This array only
+        /// contains the extra pointers, which cannot fit into AVFrame.buf. This array is always allocated using av_malloc() by whoever constructs the frame.
+        /// It is freed in av_frame_unref().
+        /// </summary>
+        public IntPtr extended_buf;
+
+        /// <summary>
+        /// Contains the number of elements in extended_buf.
+        /// </summary>
+        public int nb_extended_buf;
+
+        /// <summary>
+        /// Contains side data.
+        /// </summary>
+        public IntPtr side_data;
+
+        /// <summary>
+        /// Contains the number of elements in the side data.
+        /// </summary>
+        public int nb_side_data;
+
+        /// <summary>
+        /// Contains frame flags, a combination of @ref lavu_frame_flags.
+        /// </summary>
+        public int flags;
+
+        /// <summary>
+        /// Contains the MPEG vs JPEG YUV color range. It must be accessed using av_frame_get_color_range() and av_frame_set_color_range(). When encoding
+        /// this is set by the user. When decoding this is set by libavcodec.
+        /// </summary>
+        public AVColorRange color_range;
+
+        /// <summary>
+        /// Contains the color primaries.
+        /// </summary>
+        public AVColorPrimaries color_primaries;
+
+        /// <summary>
+        /// Contains the color transfer characteristic.
+        /// </summary>
+        public AVColorTransferCharacteristic color_trc;
+
+        /// <summary>
+        /// Contains the YUV colorspace type. It must be accessed using av_frame_get_colorspace() and av_frame_set_colorspace(). When encoding this is set
+        /// by the user. When decoding this is set by libavcodec.
+        /// </summary>
+        public AVColorSpace colorspace;
+
+        /// <summary>
+        /// Contains the chroma location.
+        /// </summary>
+        public AVChromaLocation chroma_location;
+
+        /// <summary>
+        /// Contains the frame timestamp estimated using various heuristics, in stream time base. Code outside libavutil should access this field using:
+        /// av_frame_get_best_effort_timestamp(frame). When encoding this is unused. When decoding this is set by the libavcodec and can be read by the user.
+        /// </summary>
+        public long best_effort_timestamp;
+
+        /// <summary>
+        /// Contains the reordered pos from the last AVPacket that has been input into the decoder. Code outside libavutil should access this field using:
+        /// av_frame_get_pkt_pos(frame). When encoding this is unused. When decoding this can be read by the user.
+        /// </summary>
+        public long pkt_pos;
+
+        /// <summary>
+        /// Contains the duration of the corresponding packet, expressed in AVStream->time_base units, 0 if unknown. Code outside libavutil should access
+        /// this field using: av_frame_get_pkt_duration(frame). When encoding this is unused. When decoding this can be read by the user.
+        /// </summary>
+        public long pkt_duration;
+
+        /// <summary>
+        /// Contains a pointer to metadata. Code outside libavutil should access this field using: av_frame_get_metadata(frame). When encoding this is set
+        /// by the user. When decoding this is set by libavcodec.
+        /// </summary>
+        public IntPtr metadata;
+
+        /// <summary>
+        /// Contains the decode error flags of the frame, set to a combination of FF_DECODE_ERROR_xxx flags if the decoder produced a frame, but there were
+        /// errors during the decoding. Code outside libavutil should access this field using: av_frame_get_decode_error_flags(frame). When encoding this is
+        /// unused. When decoding, this is set by libavcodec and can be read by the user.
+        /// </summary>
+        public int decode_error_flags;
+
+        /// <summary>
+        /// Contains the number of audio channels, only used for audio. Code outside libavutil should access this field using: av_frame_get_channels(frame).
+        /// When encoding this is unused. When decoding this can be read by the user.
+        /// </summary>
+        public int channels;
+
+        /// <summary>
+        /// Contains the size of the corresponding packet containing the compressed frame. It must be accessed using av_frame_get_pkt_size() and
+        /// av_frame_set_pkt_size(). It is set to a negative value if unknown. When encoding this is unused. When decoding this is set by libavcodec and can
+        /// be read by the user.
+        /// </summary>
+        public int pkt_size;
+
+        /// <summary>
+        /// Contains the QP table. Not to be accessed directly from outside libavutil.
+        /// </summary>
+        public IntPtr qscale_table;
+
+        /// <summary>
+        /// Contains the QP store stride. Not to be accessed directly from outside libavutil.
+        /// </summary>
+        public int qstride;
+
+        /// <summary>
+        /// Contains the QP type. Not to be accessed directly from outside libavutil.
+        /// </summary>
+        public int qscale_type;
+
+        /// <summary>
+        /// Contains the QP table buffer. Not to be accessed directly from outside libavutil.
+        /// </summary>
+        public IntPtr qp_table_buf;
+
+        /// <summary>
+        /// Contains a reference to the AVHWFramesContext describing the frame, for hwaccel-format frames.
+        /// </summary>
+        public IntPtr hw_frames_ctx;
+
         #endregion
     }
 }
