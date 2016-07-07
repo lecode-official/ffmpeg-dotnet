d4d3ee8d39f77728f67883602611bf378c19f96a
David Neumann <david.neumann@live.com>
2016-07-07 19:40:32 +0200

    Cleaned up the code and added documentation to it.

--- Source/FFmpegDotNet/MediaStream.cs
+++ Source/FFmpegDotNet/MediaStream.cs
@@ -9,13 +9,23 @@ using System.Runtime.InteropServices;
 
 namespace FFmpegDotNet
 {
+    /// <summary>
+    /// Represents a single stream within a format context.
+    /// </summary>
     public class MediaStream
     {
         #region Constructors
 
+        /// <summary>
+        /// Initializes a new <see cref="MediaStream"/> instance.
+        /// </summary>
+        /// <param name="streamPointer">The pointer to the internal FFmpeg stream structure.</param>
         internal MediaStream(IntPtr streamPointer)
         {
+            // Stores the pointer ot the internal FFmpeg stream structure for later use
             this.StreamPointer = streamPointer;
+
+            // Converts the internal FFmpeg stream structure to a managed structure and stores it for later use
             this.InternalStream = Marshal.PtrToStructure<AVStream>(this.StreamPointer);
         }
 
@@ -23,8 +33,14 @@ namespace FFmpegDotNet
 
         #region Internal Properties
 
+        /// <summary>
+        /// Gets a pointer to the internal FFmpeg stream structure.
+        /// </summary>
         internal IntPtr StreamPointer { get; private set; }
 
+        /// <summary>
+        /// Gets the internal FFmpeg stream converted to a managed structure.
+        /// </summary>
         internal AVStream InternalStream { get; private set; }
 
         #endregion
