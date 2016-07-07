2c6f3d8e9e7f74a559357aa45e53d9ba13f8501a
David Neumann <david.neumann@live.com>
2016-07-07 12:54:34 +0200

    Created the new project for the FFmpeg wrapper.

---
+++ Source/FFmpegDotNet/Media.cs
@@ -0,0 +1,16 @@
+
+#regio Using Directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace FFmpegDotNet
+{
+    public class Media
+    {
+    }
+}
