abdc228f15c8e1578883a37433ca0e11d1e0e964
David Neumann <david.neumann@lecode.de>
2016-07-08 14:55:54 +0200

    Removed the file endings for the libraries, which should, in theory, make this library cross-platform. This still needs to be verified.

--- Source/FFmpegDotNet.Interop/Libraries.cs
+++ Source/FFmpegDotNet.Interop/Libraries.cs
@@ -12,26 +12,26 @@
          /// Contains the name of the libavcodec library. The libavcodec library provides a generic encoding/decoding framework and contains multiple
          /// decoders and encoders for audio, video and subtitle streams, and several bitstream filters.
          /// </summary>
-         public const string AVCodec = "libavcodec.so.57"; 
+         public const string AVCodec = "libavcodec"; 
 
          /// <summary>
          /// Contains the name of the libavformat library. The libavformat library provides a generic framework for multiplexing and demultiplexing (muxing
          /// and demuxing) audio, video and subtitle streams. It encompasses multiple muxers and demuxers for multimedia container formats.
          /// </summary>
-         public const string AVFormat = "libavformat.so.57"; 
+         public const string AVFormat = "libavformat"; 
 
          /// <summary>
          /// Contains the name of the libavutil library. The libavutil library is a utility library to aid portable multimedia programming. It contains
          /// safe portable string functions, random number generators, data structures, additional mathematics functions, cryptography and multimedia
          /// related functionality (like enumerations for pixel and sample formats). It is not a library for code needed by both libavcodec and libavformat.
          /// </summary>
-         public const string AVUtil = "libavutil.so.55";
+         public const string AVUtil = "libavutil";
 
          /// <summary>
          /// Contains the name of the libswscale library. The libswscale library performs highly optimized image scaling and colorspace and pixel format
          /// conversion operations. 
          /// </summary>
-         public const string SwScale = "libswscale.so.4";
+         public const string SwScale = "libswscale";
 
          #endregion
      }
